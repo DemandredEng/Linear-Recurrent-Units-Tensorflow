dfdf094a8548e662ec0bdf41012993b9260db58d
DemandredEng <uditsharma.eng@gmail.com>
2023-08-10 13:07:59 +0530

    descupdate

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 
 from distutils.core import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'LRU-tensorflow',         
   packages = ['LRU_tensorflow'],  
   version = '0.1.0',     
   license='MIT',       
-  description = 'Linear Recurrent Unit (LRU) - TensorFlow',  
+  description = 'Unofficial TensorFlow implementation of a Linear Recurrent Unit, proposed by Google Deepmind.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',  
   author = 'Udit Sharma',                  
   author_email = 'uditsharma.eng@gmail.com',     
   url = 'https://github.com/DemandredEng/Linear-Recurrent-Units-Tensorflow',   
